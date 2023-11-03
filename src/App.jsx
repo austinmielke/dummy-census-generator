1e3f708e30f176797fb6ce797a446e4befbba959
austinmielke <amielkedev@gmail.com>
2023-11-03 10:45:30 -0500

    feat: add form control and generate and reset handlers

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import Table from './Table'
 
 function App() {
   const [members, setMembers] = useState([1])
+  const [form, setForm] = useState({
+    number: 10,
+    dependents: true
+  })
 
   return (
     <div className="container prose mx-auto mt-4 text-center">
       <h1 className="text-center">Dummy Census Generator</h1>
-      <Form />
+      <Form form={form} setForm={setForm} setMembers={setMembers} />
       {members.length > 0 && <Table />}
     </div>
   )
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,4 +1,32 @@
-function Form() {
+function Form({ form, setForm, setMembers }) {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(form)
+  }
+
+  const handleReset = (e) => {
+    e.preventDefault()
+    setMembers([])
+    setForm({
+      number: 10,
+      dependents: true
+    })
+  }
+
+  const handleNumberChange = (e) => {
+    setForm({
+      ...form,
+      number: parseInt(e.target.value)
+    })
+  }
+
+  const handleCheckChange = (e) => {
+    setForm({
+      ...form,
+      dependents: e.target.checked
+    })
+  }
+
   return (
     <form className="card mx-auto mb-16 w-96 bg-neutral text-neutral-content">
       <div className="card-body">
@@ -6,11 +34,13 @@ function Form() {
           <label className="label">
             <span className="label-text">Number of Members</span>
             <input
+              name="number"
               type="number"
-              placeholder="10"
+              value={form.number}
               min="1"
               max="100"
               className="input input-bordered w-full max-w-xs"
+              onChange={handleNumberChange}
             />
           </label>
         </div>
@@ -18,13 +48,20 @@ function Form() {
           <label className="label cursor-pointer">
             <span className="label-text">Generate Dependents</span>
             <input
+              name="dependents"
               type="checkbox"
-              checked="checked"
+              checked={form.dependents}
               className="checkbox ml-4 mr-auto"
+              onChange={handleCheckChange}
             />
           </label>
         </div>
-        <button className="btn btn-success">Generate</button>
+        <button className="btn btn-success" onClick={handleSubmit}>
+          Generate
+        </button>
+        <button className="btn btn-error" onClick={handleReset}>
+          Reset
+        </button>
       </div>
     </form>
   )
